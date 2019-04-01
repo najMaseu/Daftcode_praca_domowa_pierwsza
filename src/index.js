6aaed1d42164e55ed948d0ebf42550575278f2ac
Maseu <m.wachowicz@protonmail.com>
2019-04-01 13:14:06 +0200

    druga praca domowa

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import script from "./script.js"
-script()
+import "./scss/main.scss"
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const isProduction = (process.env.NODE_ENV === 'production');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,10 +10,19 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [new HtmlWebpackPlugin(
+    {
+      template: 'src/index.html'
+    }
+  ),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CleanWebpackPlugin()
+  ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -19,6 +31,54 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /.(png|jpg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              //publicPath: 'img/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          isProduction ?
+          MiniCssExtractPlugin.loader : {
+            loader: 'style-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isProduction
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: isProduction
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isProduction
+            }
+          }
+        ]
       }
     ]
   }
